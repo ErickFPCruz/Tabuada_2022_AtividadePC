6cf795b6e912ba94fd80d948b43de5817a2b6935
ErickFPCruz <erick.cruz97@hotmail.com>
2022-05-20 20:32:51 -0300

    Faça a tabuada de qualquer número que você quiser!!!!!!!

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+double nrPrincipal, contador, produto;
+
+Console.Write("Escreva um número qualquer: ");
+nrPrincipal = Convert.ToDouble(Console.ReadLine()!);
+
+contador = -1;
+
+Console.WriteLine($"A tabuada do número {nrPrincipal} é: ");
+
+while (contador++ < 10)
+{    
+    produto = nrPrincipal * contador;
+    Console.WriteLine($"{nrPrincipal} x {contador} = {produto}");
+}
+return;
